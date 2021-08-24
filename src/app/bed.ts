11b3cc11e1238d1b5bc98c763f82ddcf8861e4a4
PsychoKRAKEN <bhuria8302@gmail.com>
2021-08-24 15:12:13 +0530

    project changes

---
+++ src/app/bed.ts
@@ -0,0 +1,11 @@
+export class Bed {
+
+    city:String | undefined;
+    hospital:String | undefined;
+    bedtype:String | undefined;
+    bookdate:String | undefined;
+    name:String | undefined;
+    pno:number | undefined;
+    message:String | undefined;
+
+}
