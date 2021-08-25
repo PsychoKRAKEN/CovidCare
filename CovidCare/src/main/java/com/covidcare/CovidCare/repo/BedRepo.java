e6996d2554cbddcea9c1bf7f1c468fab107704a6
PsychoKRAKEN <bhuria8302@gmail.com>
2021-08-25 10:06:00 +0530

    delete

--- CovidCare/src/main/java/com/covidcare/CovidCare/repo/BedRepo.java
+++
@@ -1,9 +0,0 @@
-package com.covidcare.CovidCare.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.covidcare.CovidCare.model.Bed;
-
-public interface BedRepo extends JpaRepository<Bed, Long> {
-
-}
